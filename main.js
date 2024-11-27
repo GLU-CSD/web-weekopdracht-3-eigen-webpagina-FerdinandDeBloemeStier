355aaf1fc5bd26ad5340d1ca7af82914ab7b76e1
FerdinandDeBloemeStier <arendvanwijk55@gmail.com>
2024-11-27 18:08:11 +0100

    week opdracht 3 web woensdag commit

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
 // alert("Hello WRLD!");
+
+// Selecteer de knoppen
+const spelersButton = document.getElementById('spelers-btn');
+const geschiedenisButton = document.getElementById('geschiedeis-btn');
+const muziekButton = document.getElementById('muziek-btn');
+const prijzenButton = document.getElementById('prijzen-btn');
+
+// Selecteer het element waar de tekst moet worden weergegeven
+const tekstElement = document.getElementById('tekst');
+
+
+// Voeg een klik-event toe aan de knoppen
+spelersButton.addEventListener('click', () => {
+  tekstElement.innerHTML = 'Dit is informatie over spelers.';
+});
+
+geschiedenisButton.addEventListener('click', () => {
+  tekstElement.innerHTML = 'Dit is informatie over geschiedenis.';
+});
+
+muziekButton.addEventListener('click', () => {
+  tekstElement.innerHTML = 'Dit is informatie over muziek.';
+});
+
+prijzenButton.addEventListener('click', () => {
+  tekstElement.innerHTML = 'Dit is informatie over prijzen.';
+});
